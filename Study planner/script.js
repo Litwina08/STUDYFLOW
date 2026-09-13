262dd2a971e1ff02c8df143b01d4ba503e14e974
Litwina08 <litwinaarokiam4@gmail.com>
2026-09-13 17:52:04 +0530

    Update script.js

--- Study planner/script.js
+++ Study planner/script.js
@@ -2,7 +2,7 @@ const taskInput =
     document.getElementById("taskInput");
 
 const addButton =
-    document.getElementById("addButtonj
+    document.getElementById("addButton");
 
 const taskList =
     document.getElementById("taskList");
